7b960ba87bb8c75f93ddffcb4e6adfb858c2585d
Alberto Ramos de Santana <j.albertosantana@hotmail.com>
2025-04-27 12:24:25 -0300

    Added delete product endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.UpdateProduct;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id)
+    public record DeleteProductCommandResponse(bool IsSucess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{Id}", async (Guid id, ISender sender) => 
+            {
+                var command = new DeleteProductCommand(id);
+                var result = await sender.Send(command);
+                var response = result.Adapt<DeleteProductCommandResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductCommandResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product Summary")
+            .WithDescription("Delete Product Description");
+        }
+    }
+}
