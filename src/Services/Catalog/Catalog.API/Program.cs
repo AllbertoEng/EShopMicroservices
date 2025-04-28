3f815c66d29a2d6d04492d2d65a9616689ed8776
Alberto Ramos de Santana <j.albertosantana@hotmail.com>
2025-04-28 18:19:04 -0300

    Added initial data population for the catalog database

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -10,12 +10,16 @@ builder.Services.AddMediatR(config =>
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 
+//Carter is a library for building HTTP APIs in .NET Core using a modular approach, allowing you to create reusable components and organize your code more effectively.
 builder.Services.AddCarter();
 
+//Marten is a document database library for .NET that provides a simple and efficient way to work with data in a NoSQL manner.
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 //Global exception handling
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
