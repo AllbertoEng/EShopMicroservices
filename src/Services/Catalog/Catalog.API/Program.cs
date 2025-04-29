193d6b6ce49d18cc7da1c311ac4763353f1e6a47
Alberto Ramos de Santana <j.albertosantana@hotmail.com>
2025-04-29 08:32:55 -0300

    Added health checks using AspNetCore.HealthChecks.NpgSql package to health check postgres database and AspNetCore.HealthChecks.UI.Client to get json response

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,6 +27,9 @@ if (builder.Environment.IsDevelopment())
 //Global exception handling
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,5 +37,11 @@ app.MapCarter();
 
 //Global exception handling
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions 
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
